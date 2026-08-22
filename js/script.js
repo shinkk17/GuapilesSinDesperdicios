01fc0d2eb421467c78b69b43a98c097935d0b0a9
adexander90@gmail.com <adexander90@gmail.com>
2026-08-22 10:57:27 -0600

    Implementacion del api de whatsapp

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ anio.textContent = new Date().getFullYear();
 
 emailjs.init("YKkfnCb8ZRPHygE1M");
 
-document.getElementById('register_form').addEventListener('submit', function(event) {
-    
+document.getElementById('register_form').addEventListener('submit', function (event) {
+
     event.preventDefault();
 
     const name = document.getElementById('name').value;
@@ -13,29 +13,37 @@ document.getElementById('register_form').addEventListener('submit', function(eve
     const email = document.getElementById('email').value;
     const id = document.getElementById('id').value;
     const phone_num = document.getElementById('phone_num').value;
-    
+
     const colaborator_choice = document.getElementById('colaborator_choice').value;
 
     let message = "";
 
     if (colaborator_choice == 'buyer') {
-        message = 'Hola '+ name + " " + lastname + ", nos alegra que estés interesad@ en nuestro servicio como Comprador. \n¡Muchas gracias por unirte a la causa para un Guápiles con menos desperdicios!";
+        message = 'Hola ' + name + " " + lastname + ", nos alegra que estés interesad@ en nuestro servicio como Comprador. \n¡Muchas gracias por unirte a la causa para un Guápiles con menos desperdicios!";
     } else if (colaborator_choice == 'seller') {
-        message = 'Hola '+ name + " " + lastname + ", nos alegra que estés interesad@ en nuestro servicio como Vendedor \nMuchas gracias por unirte a la causa para un Guápiles con menos desperdicios. ¡Muchos éxitos!";
+        message = 'Hola ' + name + " " + lastname + ", nos alegra que estés interesad@ en nuestro servicio como Vendedor \nMuchas gracias por unirte a la causa para un Guápiles con menos desperdicios. ¡Muchos éxitos!";
     }
 
     const templateParams = {
-        name : 'Guapiles Sin Desperdicios',
+        name: 'Guapiles Sin Desperdicios',
         message: message,
         //title : link de la pagina de inicio en vercel
-        user_email : email,
-        reply_email : 'guapilessindesperdicios'
+        user_email: email,
+        reply_email: 'guapilessindesperdicios'
     };
 
-    emailjs.send('service_ejctmdh', 'template_77a37b2', templateParams).then(function() {
+    emailjs.send('service_ejctmdh', 'template_77a37b2', templateParams).then(function () {
         alert('¡Mensaje enviado con éxito!');
         document.getElementById('register_form').reset();
-    }, function(error){
+    }, function (error) {
         alert('Error al enviar: ' + JSON.stringify(error));
     });
 });
+function SenMessage() {
+    const telefono = "50684796204";
+    const mensaje = encodeURIComponent("Hola, me gustaría obtener más información sobre el proyecto Guápiles Sin Desperdicios. ¿Podrían proporcionarme detalles adicionales?");
+
+    const urlWhatsApp = `https://wa.me/${telefono}?text=${mensaje}`;
+
+    window.open(urlWhatsApp, '_blank');
+}
